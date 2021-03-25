f2f66553afa7445598a61619e93b4a7c32a7d904
mihai.trifan <mihai.trifan@uipath.com>
2021-03-25 14:40:47 +0200

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,30 @@
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 32', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 323', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
